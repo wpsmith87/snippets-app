78803610a2cc5b865fb1e7525f196522a138b3b7
Will <wpsmith87@gmail.com>
2018-01-12 18:43:25 +0000

    commit message

--- snippets.py
+++ snippets.py
@@ -1,7 +1,11 @@
 import logging
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+
+
 
 # Stubs
 
@@ -27,3 +31,12 @@ def get(name):
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return ""
     
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
